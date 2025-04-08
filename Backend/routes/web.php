86cb5df8fc1d77971e084ce976160e42f4e5e573
TamraouiAbdelouahab <abdelwahabtamraoui@gmail.com>
2025-04-08 23:59:22 +0100

    Authentifiaction

--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/user', function () {
+    return "je suis connecté";
+})->middleware('role:admin');
