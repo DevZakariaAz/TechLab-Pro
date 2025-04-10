569a3957fa750057eee0c68e620bb73034ce3847
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-04-10 13:42:12 +0100

    use color template

--- Frontend-react/app/(auth)/login/index.tsx
+++ Frontend-react/app/(auth)/login/index.tsx
@@ -1,19 +1,15 @@
 import { useEffect, useState } from 'react';
-import {
-  StyleSheet,
-  TextInput,
-  Text,
-  TouchableOpacity,
-  View,
-  Image,
-  Alert,
-} from 'react-native';
+import { StyleSheet,TextInput,Text,TouchableOpacity,View,Image,Alert, } from 'react-native';
+import { Stack } from "expo-router";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Link, useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import { login } from '@/api/login'; // Adjust this path as needed
+import { login } from '@/api/login'; 
 import * as Google from 'expo-auth-session/providers/google';
 import * as WebBrowser from 'expo-web-browser';
+import { Colors } from '@/constants/Colors'; 
+
+const theme = Colors.light; 
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -54,8 +50,17 @@ export default function LoginPage() {
   };
 
   return (
+    <>
+      <Stack.Screen
+        options={{
+          title: "Se connecter",
+          headerTitleAlign: "center",
+          headerBackTitle: '',
+          headerBackImage: () => <></>,
+        }}
+      />
+
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>Se connecter</Text>
 
       <View style={styles.form}>
         {/* Email Input */}
@@ -127,26 +132,28 @@ export default function LoginPage() {
         </TouchableOpacity>
       </View>
     </SafeAreaView>
+    </>
+
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#fff', padding: 24 },
+  container: { flex: 1, backgroundColor: theme.background, padding: 24 },
   title: { fontSize: 22, fontWeight: 'bold', textAlign: 'center', marginBottom: 32 },
   form: { width: '100%' },
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: '#F6F6F6',
+    backgroundColor: theme.secondary, 
     borderRadius: 30,
     paddingHorizontal: 15,
     marginBottom: 15,
   },
   icon: { marginRight: 8 },
   input: { flex: 1, height: 50, fontSize: 16 },
-  forgot: { color: '#1AA39D', alignSelf: 'flex-end', marginBottom: 24, fontSize: 14 },
+  forgot: { color: theme.primary, alignSelf: 'flex-end', marginBottom: 24, fontSize: 14 },
   loginButton: {
-    backgroundColor: '#1AA39D',
+    backgroundColor: theme.primary, 
     paddingVertical: 14,
     borderRadius: 30,
     alignItems: 'center',
@@ -154,7 +161,7 @@ const styles = StyleSheet.create({
   },
   loginText: { color: '#fff', fontSize: 16, fontWeight: '600' },
   registerPrompt: { textAlign: 'center', fontSize: 14, marginBottom: 20 },
-  registerLink: { color: '#1AA39D', fontWeight: '500' },
+  registerLink: { color: theme.primary, fontWeight: '500' },
   dividerContainer: {
     flexDirection: 'row',
     alignItems: 'center',
