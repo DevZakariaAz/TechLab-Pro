ef583c374cebf2f9fd970eff8425a04a4c639fe5
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-08-25 19:04:29 +0100

    add function in login api logic

--- Frontend-react/api/login.ts
+++ Frontend-react/api/login.ts
@@ -1,25 +1,25 @@
 export const login = async (email: string, password: string) => {
   try {
-    const response = await fetch('http://127.0.0.1:8000/api/login', {
-      method: 'POST',
+    const response = await fetch("http://127.0.0.1:8000/api/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ email, password }),
-    });
+    })
 
-    const data = await response.json();
+    const data = await response.json()
 
     if (response.ok && data.status) {
       return {
         success: true,
         token: data.token,
         user: data.user,
-      };
+      }
     } else {
-      return { success: false, message: data.message };
+      return { success: false, message: data.message }
     }
   } catch (error) {
-    return { success: false, message: 'Erreur serveur ou réseau.' };
+    return { success: false, message: "Erreur serveur ou réseau." }
   }
-};
+}
