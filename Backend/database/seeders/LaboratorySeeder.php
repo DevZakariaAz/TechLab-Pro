e68e60d3e765407138b5284e7701c44ad52eed94
Dev Zakaria <Azizi.zakaria.solicode@gmail.com>
2025-08-29 20:32:56 +0100

    seeders of laboratiore

--- Backend/database/seeders/LaboratorySeeder.php
+++ Backend/database/seeders/LaboratorySeeder.php
@@ -19,5 +19,18 @@ class LaboratorySeeder extends Seeder
             'image' => 'chemistry_lab.jpg',
             'description' => 'Chemical reactions and experiments with safety.',
         ]);
+
+        Laboratory::create([
+            'id' => 2,
+            'title' => 'Biology Lab',
+            'image' => 'biology_lab.jpg',
+            'description' => 'Study of living organisms and biological processes.',
+        ]);
+        Laboratory::create([
+            'id' => 3,
+            'title' => 'Physics Lab',
+            'image' => 'physics_lab.jpg',
+            'description' => 'Experiments in mechanics, optics, and electromagnetism.',
+        ]);
     }
 }
